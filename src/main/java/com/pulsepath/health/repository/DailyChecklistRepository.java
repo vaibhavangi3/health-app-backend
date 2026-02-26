14438f13c76dbe908bad6df4f7832b37f3a194e6
vaibhavangi3 <vaibhavangi6119@gmail.com>
2026-02-27 00:34:39 +0530

    New commits

--- src/main/java/com/pulsepath/health/repository/DailyChecklistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pulsepath.health.repository;
-
-import com.pulsepath.health.model.DailyChecklist;
-import java.time.LocalDate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DailyChecklistRepository extends JpaRepository<DailyChecklist, LocalDate> {}
